27d0ab1ea1470ac84551abce504cdeaefe978db4
pedrocel <pedrinhonovais10@gmail.com>
2025-02-13 19:46:27 -0300

    feat validation image

--- resources/views/student/profile/index.blade.php
+++ resources/views/student/profile/index.blade.php
@@ -123,25 +123,15 @@
                 <h3 class="font-manrope font-bold text-4xl text-gray-900 mb-1 max-sm:text-center">{{$user->name}}</h3>
 
 
-                @if ($user->status == 0)
-<button id="openModal" 
-    class="py-3.5 px-5 flex rounded-full bg-orange-500 items-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-orange-600">
-    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11.3011 8.69881L8.17808 11.8219M8.62402 12.5906L8.79264 12.8819C10.3882 15.6378 11.1859 17.0157 12.2575 16.9066C13.3291 16.7974 13.8326 15.2869 14.8397 12.2658L16.2842 7.93214C17.2041 5.17249 17.6641 3.79266 16.9357 3.0643C16.2073 2.33594 14.8275 2.79588 12.0679 3.71577L7.73416 5.16033C4.71311 6.16735 3.20259 6.67086 3.09342 7.74246C2.98425 8.81406 4.36221 9.61183 7.11813 11.2074L7.40938 11.376C7.79182 11.5974 7.98303 11.7081 8.13747 11.8625C8.29191 12.017 8.40261 12.2082 8.62402 12.5906Z"
-            stroke="white" stroke-width="1.6" stroke-linecap="round" />
-    </svg>
-    <span class="px-2 font-semibold text-base leading-7 text-white">Biometria Facial - Pendente de envio</span>
-</button>
-                @elseif($user->status == 2)
+                @if (!$user->facial_image_base64)
                 <button id="openModal" 
-                class=" mt-4 mr-4 py-3.5 px-5 flex rounded-full bg-orange-500 items-center shadow-sm transition-all duration-500 hover:bg-orange-600">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    class="py-3.5 px-5 flex rounded-full bg-orange-500 items-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-orange-600">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M11.3011 8.69881L8.17808 11.8219M8.62402 12.5906L8.79264 12.8819C10.3882 15.6378 11.1859 17.0157 12.2575 16.9066C13.3291 16.7974 13.8326 15.2869 14.8397 12.2658L16.2842 7.93214C17.2041 5.17249 17.6641 3.79266 16.9357 3.0643C16.2073 2.33594 14.8275 2.79588 12.0679 3.71577L7.73416 5.16033C4.71311 6.16735 3.20259 6.67086 3.09342 7.74246C2.98425 8.81406 4.36221 9.61183 7.11813 11.2074L7.40938 11.376C7.79182 11.5974 7.98303 11.7081 8.13747 11.8625C8.29191 12.017 8.40261 12.2082 8.62402 12.5906Z"
                             stroke="white" stroke-width="1.6" stroke-linecap="round" />
                     </svg>
-                    <span class="px-2 font-semibold text-base leading-7 text-white">Biometria Facial - Aguardando análise</span>
+                    <span class="px-2 font-semibold text-base leading-7 text-white">Biometria Facial - Pendente de envio</span>
                 </button>
-                @elseif($user->status == 3)
                 <button id="openModal" 
                     class="py-3.5 px-5 flex rounded-full bg-red-500 items-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-red-600">
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -150,7 +140,7 @@
                     </svg>
                     <span class="px-2 font-semibold text-base leading-7 text-white">Biometria Facial - Imagem recusada</span>
                 </button>
-                @elseif($user->status == 4)
+                @elseif($user->facial_image_base64)
                 <button id="openModal" 
                     class="py-3.5 px-5 flex rounded-full bg-green-500 items-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-green-600">
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
