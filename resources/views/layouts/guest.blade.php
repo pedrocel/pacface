c2ecb75a86e1575933fdc87b0928e521a2069a6a
pedrocel <pedrinhonovais10@gmail.com>
2025-02-18 15:41:41 -0300

    fix: icon login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,10 +15,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#000]">
             <div>
                 <a href="/">
-                    <img src="{{ asset('img/pacface.png') }}" alt="Pacface Logo">
+                    <img src="{{ asset('https://api-eventos.pacsafe.com.br/logo-branca-1024x500.png') }}" width="500" alt="Pacface Logo">
                 </a>
             </div>
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
