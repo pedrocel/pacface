0e098d868f5ca24feb1f7363ed5a8b59c2eb578c
pedrocel <pedrinhonovais10@gmail.com>
2025-02-21 11:32:26 -0300

    update dashboard

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -12,12 +12,13 @@ class DashboardController extends Controller
     public function index()
     {
         $organizacao = UserOrganizationModel::where('user_id', Auth::user()->id)->with('organization')->first();
+        $org = UserOrganizationModel::where('user_id', Auth::user()->id)->with('organization')->first();
         $user = Auth::user();
 
         if(!$user->facial_image_base64){
-            return view('student.profile.index', compact('user', 'organizacao'));
+            return view('student.profile.index', compact('user', 'organizacao', 'org'));
         }else{
-            return view('student.dashboard', compact('user', 'organizacao'));
+            return view('student.dashboard', compact('user', 'organizacao', 'org'));
         }
     }
 }
