e0a7382369820409d937964d2a319afb4ecee1ac
pedrocel <pedrinhonovais10@gmail.com>
2025-02-24 14:28:55 -0300

    Feat: Notification Modulo

---
+++ app/Http/Controllers/Director/SnackController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Director;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SnackController extends Controller
+{
+    public function getDashboard(){
+        
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\NotificationTestController;
 use App\Http\Controllers\FaceEventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/webhook/face-event', action: [FaceEventController::class, 'handleWebhook']);
 Route::get('/crawler/user-face', action: [FaceEventController::class, 'getUsers']);
+Route::post('/test-notification', [NotificationTestController::class, 'sendTestNotification']);
