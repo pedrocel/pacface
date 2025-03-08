92e7e364dcea9c65003b6c159b1cc1c2b035b2b8
pedrocel <pedrinhonovais10@gmail.com>
2025-03-08 20:16:51 -0300

    as

--- database/migrations/2025_03_08_201524_add_ip_device_table.php
+++ database/migrations/2025_03_08_201524_add_ip_device_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('user_face', function (Blueprint $table) {
-            $table->string('ip_device')->nullable();a
+            $table->string('ip_device')->nullable();
         });
     }
 
