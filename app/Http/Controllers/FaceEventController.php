d933ad17347c32585a4e446f7128c2a226de2f7c
pedrocel <pedrinhonovais10@gmail.com>
2025-03-08 21:49:57 -0300

    teste

--- app/Http/Controllers/FaceEventController.php
+++ app/Http/Controllers/FaceEventController.php
@@ -25,38 +25,43 @@ class FaceEventController extends Controller
             'date' => $request->date,
         ]);
 
-        try {
-            $client = new Client();
-            $user = User::where('id', $request->personID)->first();
-            $sala = RoomModel::where('ip_device',  $request->ip)->first();
+        $user = User::where('id', $request->personID)->first();
 
-            // Configurações da API do ChatPro
-            $instanceId = 'chatpro-1aors879o7';
-            $token = '66rvryi9wasim03a63ljr8cmyloby8';
-            $url = "https://v5.chatpro.com.br/{$instanceId}/api/v1/send_message";
-
-            // Faz a requisição para a API do ChatPro
-            $response = $client->request('POST', $url, [
-                'json' => [
-                    'number' => '5582988291668', // Número do usuário no formato internacional
-                    'message' => 'O Aluno '. $user->name.' foi identificado na: '.$sala->name. '. Data do registro: '. $request->date, // Mensagem da notificação
-                ],
-                'headers' => [
-                    'Authorization' => $token, // Adiciona o prefixo Bearer
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json',
-                ],
-            ]);
-        } catch (RequestException $e) {
-            // Captura erros de requisição
-            if ($e->hasResponse()) {
-                $response = $e->getResponse();
-                $errorMessage = "Erro ao enviar WhatsApp para  " . $response->getStatusCode() . " - " . $response->getBody();
-            } else {
-                $errorMessage = "Erro ao enviar WhatsApp para  " . $e->getMessage();
+        if($user){
+            try {
+                $client = new Client();
+                $user = User::where('id', $request->personID)->first();
+                $sala = RoomModel::where('ip_device',  $request->ip)->first();
+    
+                // Configurações da API do ChatPro
+                $instanceId = 'chatpro-1aors879o7';
+                $token = '66rvryi9wasim03a63ljr8cmyloby8';
+                $url = "https://v5.chatpro.com.br/{$instanceId}/api/v1/send_message";
+    
+                // Faz a requisição para a API do ChatPro
+                $response = $client->request('POST', $url, [
+                    'json' => [
+                        'number' => '5582988291668', // Número do usuário no formato internacional
+                        'message' => 'O Aluno '. $user->name.' foi identificado na: '.$sala->name. '. Data do registro: '. $request->date, // Mensagem da notificação
+                    ],
+                    'headers' => [
+                        'Authorization' => $token, // Adiciona o prefixo Bearer
+                        'Accept' => 'application/json',
+                        'Content-Type' => 'application/json',
+                    ],
+                ]);
+            } catch (RequestException $e) {
+                // Captura erros de requisição
+                if ($e->hasResponse()) {
+                    $response = $e->getResponse();
+                    $errorMessage = "Erro ao enviar WhatsApp para  " . $response->getStatusCode() . " - " . $response->getBody();
+                } else {
+                    $errorMessage = "Erro ao enviar WhatsApp para  " . $e->getMessage();
+                }
+               
             }
-           
         }
+      
 
         return response()->json(['message' => 'Face event created successfully'], 201);
     }
