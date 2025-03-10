072766e090a6c982c20e1f0209ab35a814f01b5d
pedrocel <32284437+pedrocel@users.noreply.github.com>
2025-03-10 10:39:24 -0300

    teste

--- resources/views/director/layout.blade.php
+++ resources/views/director/layout.blade.php
@@ -422,7 +422,15 @@
                     <a href="{{ route('director.frequency.dashboard.get') }}" class="menu-item flex items-center gap-2 p-2 rounded hover:bg-emerald-700 group transition-colors
                     {{ request()->routeIs('director.frequency.dashboard.get') ? 'text-gray-100 bg-emerald-700 shadow-lg' : 'text-white' }}">
                         <div class="p-1.5 bg-white/10 rounded group-hover:bg-white/20 transition-colors">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M22 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect><line x1="16" x2="16" y1="2" y2="6"></line><line x1="8" x2="8" y1="2" y2="6"></line><line x1="3" x2="21" y1="10" y2="10"></line></svg>
+                        </div>
+                        <span>Frequência escolar</span>
+                    </a>
+
+                    <a href="{{ route('director.discipline.index') }}" class="menu-item flex items-center gap-2 p-2 rounded hover:bg-emerald-700 group transition-colors
+                    {{ request()->routeIs('director.discipline.index') ? 'text-gray-100 bg-emerald-700 shadow-lg' : 'text-white' }}">
+                        <div class="p-1.5 bg-white/10 rounded group-hover:bg-white/20 transition-colors">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect><line x1="16" x2="16" y1="2" y2="6"></line><line x1="8" x2="8" y1="2" y2="6"></line><line x1="3" x2="21" y1="10" y2="10"></line></svg>
                         </div>
                         <span>Frequência escolar</span>
                     </a>
