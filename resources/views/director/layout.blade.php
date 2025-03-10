059ec9f61b8a94d79f29220f806e36215e1fff0c
pedrocel <32284437+pedrocel@users.noreply.github.com>
2025-03-10 10:21:28 -0300

    pfa ve se fpo

--- resources/views/director/layout.blade.php
+++ resources/views/director/layout.blade.php
@@ -410,8 +410,10 @@
                         </div>
                         <span>Dashboard</span>
                     </a>
-                    <a href="{{ route('director.class.index') }}" class="menu-item flex items-center gap-2 p-2 rounded bg-emerald-700 group">
-                        <div class="p-1.5 bg-white/20 rounded">
+                    <a href="{{ route('director.class.index') }}" class="menu-item flex items-center gap-2 p-2 rounded bg-emerald-700 group
+                    {{ request()->routeIs('director.class.index') ? 'text-gray-100 bg-emerald-700 shadow-lg' : 'text-gray-300' }}">
+                        
+                    <div class="p-1.5 bg-white/20 rounded">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M22 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                         </div>
                         <span>Turmas</span>
