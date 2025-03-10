7a0512320043498144a8fa50c1ad63870380c40b
pedrocel <32284437+pedrocel@users.noreply.github.com>
2025-03-10 10:35:37 -0300

    feat: intion

--- resources/views/director/layout.blade.php
+++ resources/views/director/layout.blade.php
@@ -420,7 +420,7 @@
                     </a>
 
                     <a href="{{ route('director.frequency.dashboard.get') }}" class="menu-item flex items-center gap-2 p-2 rounded hover:bg-emerald-700 group transition-colors
-                    {{ request()->routeIs('director.class.index') ? 'text-gray-100 bg-emerald-700 shadow-lg' : 'text-white' }}">
+                    {{ request()->routeIs('director.frequency.dashboard.get') ? 'text-gray-100 bg-emerald-700 shadow-lg' : 'text-white' }}">
                         <div class="p-1.5 bg-white/10 rounded group-hover:bg-white/20 transition-colors">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M22 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                         </div>
